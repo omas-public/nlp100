74d876107de9d6ce245d074aeb946461cd547865
Omas Naohiko <omas@std.it-collge.ac.jp>
2020-06-15 15:45:44 +0900

    modify ch1.f05.py

--- src/ch1.f05.py
+++ src/ch1.f05.py
@@ -1,10 +1,9 @@
 def f05(target):
-  str2charlist = lambda s: [c for c in s]
-  str2wordlist = lambda s: [w for w in s.split()]
+  str2charlist = lambda s: list(s)
+  str2wordlist = lambda s: s.split()
   ngram = lambda n: lambda tl: [''.join(tl[i:i + n]) for i in range(len(tl) - n + 1)]
  
   bigram = ngram(2)
   print(bigram(str2charlist(target)))
   print(bigram(str2wordlist(target)))
 
-f05('I am an NLPer')
